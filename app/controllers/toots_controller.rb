9cd16530b4335a26bb94c1c475c535f11f469ea7
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-07-12 13:29:54 +0900

    add toots_controller.rb

---
+++ app/controllers/toots_controller.rb
@@ -0,0 +1,17 @@
+class TootsController < ApplicationController
+    before_action :set_topic
+
+    def create
+        comment = @topic.toots.create! toots_params
+        redirect_to @topic
+    end
+
+    private
+        def set_topic
+            @topic = Topic.find(params[:topic_id])
+        end
+
+        def toots_params
+            params.required(:toot).permit(:content)
+        end
+end

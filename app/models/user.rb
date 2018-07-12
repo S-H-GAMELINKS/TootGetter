c7252834d458e4982051bb71cd4e27035f7d7d28
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-07-12 15:09:21 +0900

    add method'S

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+
+  def self.find_for_oauth(auth)
+    user = User.where(email: auth.uid, provider: auth.provider).first
+    user.update!(uid: auth.extra.raw_info.display_name)
+            
+    unless user
+      user = User.create(
+        uid:      auth.extra.raw_info.display_name,
+        provider: auth.provider,
+        email:    User.dummy_email(auth),
+        password: Devise.friendly_token[0, 20]
+      )
+      user.save!
+    end
+    current_user = user
+  end
+            
+  private
+            
+  def self.dummy_email(auth)
+    "#{auth.uid}"
+  end
 end

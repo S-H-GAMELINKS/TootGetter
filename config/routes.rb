804c0f8bf6aa85dd206a0c420536aac70b007959
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-07-12 15:15:45 +0900

    fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'topics#index'
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :topics do
     resources :toots
   end

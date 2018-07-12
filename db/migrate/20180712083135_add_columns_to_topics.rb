5f1efd754e214cacffe98320b0277cbc99abc83d
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-07-12 17:32:44 +0900

    add default

--- db/migrate/20180712083135_add_columns_to_topics.rb
+++ db/migrate/20180712083135_add_columns_to_topics.rb
@@ -1,5 +1,5 @@
 class AddColumnsToTopics < ActiveRecord::Migration[5.2]
   def change
-    add_column :topics, :private, :boolean
+    add_column :topics, :private, :boolean, default: false, null: false
   end
 end

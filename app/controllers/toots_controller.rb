decdc9d41960cab901d557c2915dd89b9165a1d6
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-07-12 16:26:09 +0900

    add comment

--- app/controllers/toots_controller.rb
+++ app/controllers/toots_controller.rb
@@ -1,6 +1,6 @@
 class TootsController < ApplicationController
     before_action :set_topic
-    protect_from_forgery :except => [:create]
+    protect_from_forgery :except => [:create] # TODO CSRF対策をオフにしているだけなので対応策を考える
 
     def create
         comment = @topic.toots.create! toots_params

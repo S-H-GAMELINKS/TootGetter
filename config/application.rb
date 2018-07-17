66d2b8e79d4ee3912d345dd7726087632beff76b
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-07-17 15:37:03 +0900

    ja to en

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Tootgetter
 
     Rails.configuration.action_controller.per_form_csrf_tokens = true
     
-    config.i18n.default_locale = :ja
+    config.i18n.default_locale = :en
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers

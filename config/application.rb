4da3cde9b687ea38c155a5ed1cd9db08efa0675e
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-07-21 23:13:41 +0900

    using slim

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module Tootgetter
     
     config.i18n.default_locale = :ja
 
+    config.generators.template_engine = :slim  
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
